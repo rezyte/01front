8bccbbb3b10cccd4beaf0ece01f48fc667af5651
rezyte <erfangale@gmail.com>
2020-12-19 01:29:15 -0800

    ''

--- js/components/landingPAge/signupAndWorker/worker/worker.vue
+++ js/components/landingPAge/signupAndWorker/worker/worker.vue
@@ -7,7 +7,7 @@
                 </div>
                 <div class="aditional culText firstShow">
                     <p>
-                       {{gender}}  {{name}} {{fname}} شما  با پر کردن فرم زیر میتوانید از خدمات فروش و ابزار های اماده شده ی صنف ماشین سازی استفاده نمایید
+                       {{fullname}}   با پر کردن فرم زیر میتوانید از خدمات فروش و ابزار های اماده شده ی صنف ماشین سازی استفاده نمایید
                     </p>
                 </div>
             </div>
@@ -23,39 +23,82 @@ name: "worker",
             name:"",
             fname:"",
             gender:"",
+          fullname:""
 
         }
     },
-    mounted() {
+    created() {
         const urlParams = new URLSearchParams(window.location.search);
-        const name = urlParams.get('name');
-        const fname = urlParams.get('fname');
+        let name = urlParams.get('name');
+        let fname = urlParams.get('fname');
+        if(fname==null){
+          fname=""
+        }
+        if(name==null){
+          name=""
+        }
       const gender = urlParams.get('gender');
-      console.log("gender",gender)
-      // if(gender==null){
-      //   this.gender="شما"
-      // }
-      switch (gender){
-        case "female":
-          if(fname==null && name==null){
-            this.gender='سرکار خانم مهندس شما'
-          }else{
-            this.gender='سرکار خانم مهندس'
-          }
-
-              break;
-        case "male":
-          this.gender="جناب مهندس"
-              break
-        default:
-          this.gender=""
+      this.fname=fname
+      this.name=name
+      if(gender==null){
+        this.fullname="مهندس"+" "+fname+"" + name+" "+"شما"
       }
-        if(name!=null){
-            this.name=name
-        }
-        if(fname!=null){
-            this.fname=fname
-        }
+      if(gender=="female"){
+        this.fullname="سرکار خانم مهندس" +" "+fname+"" + name+" "+"شما"
+      }
+      if(gender=="male"){
+        this.fullname="جناب مهندس" +" "+fname+"" + name+" "+"شما"
+      }
+
+      if(gender==null && name=='' && fname==''){
+        this.fullname='َشما'
+      }
+
+
+
+      // console.log("gender",gender)
+      // // if(gender==null){
+      // //   this.gender="شما"
+      // // }
+      // switch (gender){
+      //   case "female":
+      //     if(fname==null && name==null){
+      //       this.gender='سرکار خانم مهندس'
+      //     }else{
+      //       this.gender='سرکار خانم مهندس'
+      //     }
+      //
+      //         break;
+      //   case "male":
+      //     this.gender="جناب مهندس"
+      //         break
+      //   default:
+      //     this.gender=""
+      // }
+      //   if(name!=null){
+      //     if(gender==null){
+      //       this.name=" مهندس "+name ;
+      //
+      //     }else{
+      //       this.name=name;
+      //     }
+      //
+      //
+      //   }
+      //   if(fname!=null){
+      //     if(name==null && gender=='male'){
+      //       this.fname="جناب مهندس "+fname
+      //     }else if(name==null && gender=='female'){
+      //       this.fname="سرکار خانم مهندس "+fname
+      //     }
+      //
+      //     else{
+      //
+      //       this.fname=fname
+      //     }
+      //
+      //   }
+      //   console.log(fname,name,gender)
     }
 }
 </script>
