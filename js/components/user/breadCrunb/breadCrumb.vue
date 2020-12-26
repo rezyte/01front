52b9d977bca0caa166f49a19cf136be0ff3614c1
rezyte <erfangale@gmail.com>
2020-12-26 03:14:34 -0800

    ''

--- js/components/user/breadCrunb/breadCrumb.vue
+++ js/components/user/breadCrunb/breadCrumb.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="breadCrumb">
+  <div id="breadCrumb" ref="bread">
     <ul v-if="type==='product'">
       <li>
         <a :href="'/products/product-detial/'+JSON.parse(product).slug">{{ JSON.parse(product).title }}</a>
 
       </li>
       <li>
-        <a :href="'/product-category/'+JSON.parse(product).category[0].slug"><span>/</span>{{ JSON.parse(product).category[0].title }}</a>
+        <a :href="'/product-category/'+JSON.parse(product).category[0].slug"><span>/</span>{{
+            JSON.parse(product).category[0].title
+          }}</a>
       </li>
       <li>
         <a href="/"><span>/</span>خانه</a>
@@ -16,15 +18,14 @@
     <ul v-if="type==='category'">
 
       <li>
-        <a :href="'/product-category/'+JSON.parse(products)[0].category[0].slug">{{ JSON.parse(products)[0].category[0].title }}</a>
+        <a :href="'/product-category/'+JSON.parse(products)[0].category[0].slug">{{
+            JSON.parse(products)[0].category[0].title
+          }}</a>
       </li>
       <li>
         <a href="/"><span>/</span>خانه</a>
       </li>
     </ul>
-
-
-
 
 
   </div>
@@ -33,10 +34,22 @@
 <script>
 export default {
   name: "breadCrumb",
-  props: ['product', 'type','products'],
+  props: ['product', 'type', 'products'],
   created() {
-    console.log(JSON.parse(this.products)[0],'this.products')
-  }
+    console.log(JSON.parse(this.products)[0], 'this.products')
+  },
+  mounted() {
+    const bread = this.$refs.bread
+    this.$nextTick(() => {
+      setTimeout(() => {
+        bread.scrollLeft = 5000
+        document.body.addEventListener('resize', () => {
+          bread.scrollLeft = 5000
+        })
+      }, 100)
+    })
+
+  },
 }
 </script>
 
@@ -44,12 +57,19 @@ export default {
 #breadCrumb {
   width: 100%;
   margin-top: 30px;
+  overflow: auto;
+  justify-content: flex-end;
+  display: flex;
+  scroll-margin-inline: 5000px;
 }
 
 #breadCrumb ul {
   display: flex;
   justify-content: flex-end;
   margin-right: 20px;
+  width: max-content;
+  height: 30px;
+  overflow: auto;
 }
 
 a {
