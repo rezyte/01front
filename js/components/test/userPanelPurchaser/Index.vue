4d5340da9ef6ec5f4a6ae0dccfe309b7ae9d0ade
mehran7775 <65393236+mehran7775@users.noreply.github.com>
2021-04-20 11:35:13 +0430

    create directre panel for userPanelPurchaser

---
+++ js/components/test/userPanelPurchaser/Index.vue
@@ -0,0 +1,14 @@
+<template>
+ <slot>
+ </slot>
+</template>
+
+<script>
+export default {
+name: "Index"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ js/components/test/userPanelPurchaser/pages/Info.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+name: "Info"
+}
+</script>
+
+<style scoped>
+
+</style>
